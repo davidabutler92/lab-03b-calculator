02939e6b44e076eed140a8f9b8b991b366006ab0
davidabutler92 <davidabutler92@yahoo.com>
2020-09-24 12:03:40 -0700

    add multilpy function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//GET
 const addNum1 = document.getElementById('addInput-1');
 const addNum2 = document.getElementById('addInput-2');
 const addButton = document.getElementById('add-btn');
@@ -8,6 +9,12 @@ const subNum2 = document.getElementById('subInput-2');
 const subButton = document.getElementById('sub-btn');
 const subTextResults = document.getElementById('sub-results');
 
+const multNum1 = document.getElementById('multInput-1');
+const multNum2 = document.getElementById('multInput-2');
+const multButton = document.getElementById('mult-btn');
+const multTextResults = document.getElementById('mult-results');
+
+//calculator functions 
 const add = (num1, num2) => {
   let add = num1 + num2;
   return  add;
@@ -18,9 +25,15 @@ const subtract = (num1, num2) => {
   return  sub;
 }
 
+const multiply = (num1, num2) => {
+  let mult = num1 * num2;
+  return  mult;
+}
+
+//SET
 addButton.addEventListener('click', () => {
   const results = add(addNum1.valueAsNumber, addNum2.valueAsNumber);
-  subTextResults.textContent = results;
+  addTextResults.textContent = results;
   console.log(results);
 });
 
@@ -30,3 +43,8 @@ subButton.addEventListener('click', () => {
   console.log(results);
 });
 
+multButton.addEventListener('click', () => {
+  const results = multiply(multNum1.valueAsNumber, multNum2.valueAsNumber);
+  multTextResults.textContent = results;
+  console.log(results);
+});
